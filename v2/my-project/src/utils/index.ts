955cb6e6139c255d57ae2625466b8976042c8bf4
vasujain5718 <jain90662@gmail.com>
2025-06-19 16:48:13 +0530

    removed garbage folders

--- v2/my-project/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export function log(message: string): void {
-    console.log(message);
-}
-
-export function errorHandler(error: Error): void {
-    console.error('An error occurred:', error.message);
-}
